5515d213734d25573f4970719c915dac5a978d7c
Glory Agatevure <agatevureglory@gmail.com>
2022-09-16 01:08:16 +0100

    add cron job

---
+++ src/cronjob.js
@@ -0,0 +1,46 @@
+import axios from "axios"
+
+export default function getTVLDetails(){
+  const slackCall = async (message) => {
+    const slackWebHookUrl = `https://hooks.slack.com/services/${process.env.REACT_APP_SLACK_HOOK}`
+    const slackBody = {
+      type: "mrkdwn",
+      text: message
+    }
+    try{
+      const response = await axios.post(slackWebHookUrl, JSON.stringify(slackBody), {
+        withCredentials: false,
+        transformRequest: [(data, headers) => {
+          delete headers.post["Content-Type"]
+          return data
+        }]
+      })
+      console.log(` slack call ${response}`)
+  
+    }catch(err){
+  
+    }
+  }
+
+  const getTVL = async() => {
+    const tvlUrl = "https://asia-southeast1-algomint-tvl.cloudfunctions.net/tvlSummary"
+    try{
+      const response = await axios.get(tvlUrl)
+      slackCall(`
+        ${response.data.BTC} BTC, 
+        ${response.data.WBTC} WBTC,
+        ${response.data.ETH} ETH,
+        ${response.data.SIPLP} goUSD/USDC LP,
+        ${response.data.USDC} USDC,
+        ${response.data.goBTC} goBTC,
+        ${response.data.goETH} goETH,
+        ${response.data.goUSD} goUSD,
+      `)
+      return response.data
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  getTVL()
+}
