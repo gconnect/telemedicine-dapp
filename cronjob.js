c2ed58c9a090b0cc4e98bb02c140339d953326cf
Glory Agatevure <agatevureglory@gmail.com>
2022-09-17 11:08:33 +0100

    reorder the call functions

--- cronjob.js
+++ cronjob.js
@@ -1,39 +1,36 @@
 const axios =  require("axios")
 require('dotenv').config({path: './.env'});
 
-  const slackCall = async (message) => {
+const getTVL = async() => {
+  const tvlUrl = "https://asia-southeast1-algomint-tvl.cloudfunctions.net/tvlSummary"
+  try{
+    const response = await axios.get(tvlUrl)
+    return response.data
+  }catch(error){
+    console.error(error)
+  }
+}
+  const slackMessage = async () => {
     const slackWebHookUrl = `https://hooks.slack.com/services/${process.env.REACT_APP_SLACK_HOOK}`
+
+    const tvlData = await getTVL()
+    console.log(tvlData)
+
     const slackBody = {
       type: "mrkdwn",
-      text: message
+      text: ` ${parseFloat(tvlData.BTC).toFixed(2)} BTC, ${parseFloat(tvlData.WBTC).toFixed(2)} WBTC, ${parseFloat(tvlData.ETH).toFixed(2)} ETH, ${parseFloat(tvlData.SIPLP).toFixed(2)} goUSD/USDC LP, ${parseFloat(tvlData.USDC).toFixed(2)} USDC, ${parseFloat(tvlData.goBTC).toFixed(2)} goBTC, ${parseFloat(tvlData.goETH).toFixed(2)} goETH, ${parseFloat(tvlData.goUSD).toFixed(2)} goUSD,`
     }
     try{
-      const response = await axios.post(slackWebHookUrl, JSON.stringify(slackBody), {
+      await axios.post(slackWebHookUrl, JSON.stringify(slackBody), {
         withCredentials: false,
         transformRequest: [(data, headers) => {
           delete headers.post["Content-Type"]
           return data
         }]
       })
-      console.log(` slack call ${response}`)
-      return response
     }catch(err){
       console.error(err)
     }
   }
 
-  const getTVL = async() => {
-    const tvlUrl = "https://asia-southeast1-algomint-tvl.cloudfunctions.net/tvlSummary"
-    try{
-      const response = await axios.get(tvlUrl)
-      await slackCall(`
-        ${parseFloat(response.data.BTC).toFixed(2)} BTC, ${parseFloat(response.data.WBTC).toFixed(2)} WBTC, ${parseFloat(response.data.ETH).toFixed(2)} ETH, ${parseFloat(response.data.SIPLP).toFixed(2)} goUSD/USDC LP, ${parseFloat(response.data.USDC).toFixed(2)} USDC, ${parseFloat(response.data.goBTC).toFixed(2)} goBTC, ${parseFloat(response.data.goETH).toFixed(2)} goETH, ${parseFloat(response.data.goUSD).toFixed(2)} goUSD,
-      `)
-      console.log(response.data)
-      return response.data
-    }catch(error){
-      console.error(error)
-    }
-  }
-
-  getTVL()
+  slackMessage()
